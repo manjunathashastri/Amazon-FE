37fabc821f4ff94cb7d209bf7401f1f9df8c73e0
manjunathashastri <manjunathashastri01@gmail.com>
2024-10-22 23:12:47 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "abc-mega-project-s3bucket"  
+    bucket = "abcd-mega-project-s3bucket"  
     key    = "EKS/terraform.tfstate"  
     region = "ap-south-1" 
   }
